941ac086b4140073e48f63d6754d48f0a7c9cf71
creed28 <hristo.zagorliev44@gmail.com>
2024-01-17 17:26:16 +0200

    Make navbar responsive + add more links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,34 +1,91 @@
 'use client'
 
 import Link from 'next/link';
-import Image from 'next/image';
+import Logo from './Logo';
 import LinkItem from './LinkItem';
+import { usePathname } from 'next/navigation';
+import { FaBars } from "react-icons/fa";
+import { useState } from 'react';
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <nav className='bg-[#F4EDE4] w-[100%] blur-[10%] z-[1]'>
-      <div className='flex p-2 max-w-screen-md mx-auto flex-wrap items-center justify-between'> 
-        <Link href="/" className='flex items-center mr-5'>
-          <h1 className='logoBox tracking-tight'>
-            <Image 
-              src="/assets/images/coffee-logo.png" 
-              alt='Coffee logo' 
-              width={25} 
-              height={25}               
-            />
-            <p className='font-Rounded1c text-[#1A202C]'>
-              Hristo Zagorliev
-            </p>
-          </h1>
-        </Link>
-        <ul>
-          <li>
-            <LinkItem href='/about' path={'/'} content='About' />
-          </li>
-        </ul>
+    <nav className='bg-[#F4EDE4] w-[100%] blur-[10%] z-[1] max-h-[56px] items-center 
+      flex justify-center'>
+      <div className='flex gap-x-48'>
+        <div className='flex p-2 max-w-screen-md mx-auto flex-wrap items-center 
+          gap-x-5 justify-between'>
+          <Link href='/' className='flex items-center'>
+            <Logo />
+          </Link>
+          <ul className='items-center flex-grow gap-x-2 hidden w-full 
+            md:flex md:w-auto'>
+            <li>
+              <LinkItem 
+                href='/posts' 
+                path={pathname} 
+                content='Posts' />
+            </li>
+            <li>
+              <LinkItem 
+                href='/about' 
+                path={pathname} 
+                content='About' />
+            </li>
+            <li>
+              <LinkItem 
+                href='/contact' 
+                path={pathname} 
+                content='Contact Me' />
+            </li>
+            <li>
+              <LinkItem 
+                href='https://github.com/creed28/Personal-Blog' 
+                content='View Source' />
+            </li>
+          </ul>
+        </div>
+        <div className="relative mt-4 md:hidden">
+          <div
+            className={`dropdownIcon ${isDropdownOpen ? 'bg-[#EDF2F7]' : ''}`}
+            onClick={handleDropdownToggle}
+          >
+            <FaBars size={14} />
+          </div>
+          {isDropdownOpen && (
+            <div className="dropdown">
+              <Link 
+                href='/posts' 
+                className='dropdownLink'>
+                  Posts
+              </Link>
+              <Link 
+                href='/about' 
+                className='dropdownLink'>
+                  About
+              </Link>
+              <Link 
+                href='/contact' 
+                className='dropdownLink'>
+                  Contact Me
+              </Link>
+              <Link 
+                href='https://github.com/creed28/Personal-Blog' 
+                className='dropdownLink'>
+                  View Source
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

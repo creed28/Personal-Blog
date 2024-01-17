0974be8880a295485e962a58c7bf2738615ad1ad
creed28 <hristo.zagorliev44@gmail.com>
2024-01-17 17:26:59 +0200

    Optimize LinkItem component's code arrangement

--- components/LinkItem.jsx
+++ components/LinkItem.jsx
@@ -4,11 +4,14 @@ import Link from 'next/link';
 
 const LinkItem = ({ href, path, content}) => {
   const active = path === href;
-    
+
   return(
-    <Link href={href} className={`p-2 ${active ? 'bg-[glassTeal] text-[#202023]' 
-      : 'bg-[undefined] text-[gray]'}`}>          
-      {content}
+    <Link 
+      href={href} 
+      className={`linkItem
+      ${active ? 'bg-[#88CCCA]' 
+      : 'bg-[undefined]'}`}> 
+        {content}       
     </Link>
   )
 }

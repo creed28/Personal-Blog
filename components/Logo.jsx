f6fbafd7756926d863ff1eb09cf7e1ea99171ec7
creed28 <hristo.zagorliev44@gmail.com>
2024-01-17 17:25:32 +0200

    Add a separate logo component

---
+++ components/Logo.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Image from 'next/image';
+
+const Logo = () => {
+  return (
+    <h1 className='logoBox tracking-tighter'>
+      <Image 
+        src="/assets/images/coffee-logo.png" 
+        alt='Coffee logo' 
+        width={27} 
+        height={27}               
+      />
+      <p className='font-Rounded1c text-[#1A202C]'>
+        Hristo Zagorliev
+      </p>
+    </h1>
+  )
+}
+
+export default Logo

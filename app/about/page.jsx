4f8886165f8098c93c82c1ee2822ca04dbae73d2
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-01-17 12:16:12 +0200

    Add about page

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <section>
+      <div className="rounded-lg bg-[#F5F0E8] p-3 text-center mb-6">
+        Hello, I&apos;m a Computer Science student based in Bulgaria!
+      </div>
+
+      <div className="flex">
+        <div className="flex-grow">
+          <h2 className="text-[1.875rem] font-bold font-Rounded1c">
+            Hristo Zagorliev
+          </h2>
+          <p>
+            Digital Craftsman ( Web Developer / Web Designer / Bookworm )
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About

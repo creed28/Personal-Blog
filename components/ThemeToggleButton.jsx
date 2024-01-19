4bdf9952fd9528752dc58a4cb833d78583c31a72
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-01-19 12:27:07 +0200

    Add animation for toggle theme button

--- components/ThemeToggleButton.jsx
+++ components/ThemeToggleButton.jsx
@@ -1,27 +1,44 @@
-'use client'
+'use client';
 
-import Image from "next/image";
-import { useState } from "react";
+import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
+import { useState } from 'react';
 
 const ThemeToggleButton = () => {
   const [isToggle, setIsToggle] = useState(false);
 
   const toggleTheme = () => {
     setIsToggle(!isToggle);
-  }
+  };
 
   return (
-    <button 
-      className={`flex items-center ${isToggle ? "bg-[#fbd38d] hover:bg-[#F6AD55]" 
-      : "bg-[#805ad5] hover:bg-[#6B46C1]"} p-[10px] rounded-[0.375rem]`}
-      onClick={toggleTheme}>
-      <Image 
-        src={`/assets/images/${isToggle ? "sun" : "moon"}-icon.png`}
-        alt="Toggle dark theme"
-        width={20}
-        height={20} />
-    </button>
-  )
-}
+    <AnimatePresence mode='wait' initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={isToggle}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <button
+          className={`flex items-center ${
+            isToggle
+              ? 'bg-[#fbd38d] hover:bg-[#F6AD55]'
+              : 'bg-[#805ad5] hover:bg-[#6B46C1]'
+          } p-[10px] rounded-[0.375rem]`}
+          onClick={toggleTheme}
+        >
+          <Image
+            src={`/assets/images/${isToggle ? 'sun' : 'moon'}-icon.png`}
+            alt="Toggle theme"
+            width={20}
+            height={20}
+          />
+        </button>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
 export default ThemeToggleButton

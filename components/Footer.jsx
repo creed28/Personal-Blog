8b1070dec4e7f164f5c818215939d3176878e6b4
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-01-26 18:03:19 +0200

    Add footer id

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,11 @@
 
 const Footer = () => {
   return (
-    <footer className="text-center opacity-40 text-sm py-8 dark:text-[#d8d8d9]">
-      &copy; {new Date().getFullYear()} Hristo Zagorliev. 
-      All Rights Reserved.
+    <footer 
+      className="footer"
+      id="main-footer">
+        &copy; {new Date().getFullYear()} Hristo Zagorliev. 
+        All Rights Reserved.
     </footer>
   )
 }

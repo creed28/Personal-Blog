96add488f180d6dfd64d9d776e6e41c3b99d5932
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-02-18 22:25:14 +0200

    Improve 404 page design

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,23 +1,25 @@
 import Link from 'next/link';
+import SadFace from '../public/assets/icons/404';
 
 const NotFound = () => {
   return (
     <section className="container max-w-[518px] mx-auto">
-      <h1 className="text-4xl font-semibold mt-8 dark:text-[#EEEEEE] 
-      font-Rounded1c ml-6">
-        Not found
-      </h1>
-      <p className="text-[15px] mt-4 dark:text-[#EEEEEE] 
-      font-Rounded1c ml-6">
-        The page you&apos;re looking for was not found.
-      </p>
-      <hr className="my-6 mx-4" />
+      <div className='flex flex-col justify-center items-center'>
+        <SadFace />
+        <h2 className="text-4xl font-semibold mt-8 dark:text-[#EEEEEE] 
+          font-Rounded1c">
+          <span className='text-5xl'>404</span> Not found
+        </h2>
+        <p className="text-[15px] mt-4 dark:text-[#EEEEEE] 
+          font-Rounded1c">
+          Sorry, the page you&apos;re looking for doesn&apos;t exist.
+        </p>
+      </div>
+      <hr className="my-8 mx-4" />
       <div className="my-6 text-center">
         <Link 
           href="/"
-          className="inline-block px-4 py-2 bg-[#319795] hover:bg-[#2C7A7B]
-          rounded text-white dark:bg-[#81e6d9] dark:hover:bg-[#4FD1C5] 
-          font-semibold dark:text-[#244148]">
+          className="btn inline-block px-4 my-0">
           Return to home
         </Link>
       </div>

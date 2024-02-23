de057c18156acbb981e8dcaef9e1c57807f7e202
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-02-23 15:01:50 +0200

    Add logic to conditionally render MainMenu and DropDownMenu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,12 +3,22 @@
 import Link from 'next/link';
 import Logo from './Logo';
 import ThemeToggleButton from './ThemeToggleButton';
-import MainMenu from './MainMenu';
+import { useEffect, useState } from 'react';
 import DropdownMenu from './DropdownMenu';
+import MainMenu from './MainMenu';
 
 const Navbar = () => {
+  const[windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  return (
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize)
+  }, []);
+
+  return (  
     <nav className='navbar' id='main-nav'>
       <div className='flex gap-x-64 md:gap-x-32'>
         <div className='flex p-2 max-w-screen-md mx-auto flex-wrap items-center 
@@ -16,15 +26,17 @@ const Navbar = () => {
           <Link href='/' className='flex items-center shrink-0'>
             <Logo />
           </Link>
-          <MainMenu />
+          {windowWidth > 768 && <MainMenu />}
         </div>
         <div className='flex gap-x-2 mr-5 md:m-0'>
           <div className='flex items-center shrink-0'>
             <ThemeToggleButton />
           </div>
-          <div className="relative mt-4 md:hidden">
-            <DropdownMenu />
-          </div>
+          {windowWidth <= 768 && (
+            <div className="relative mt-4">
+              <DropdownMenu />
+            </div>
+          )}
         </div>
       </div>
     </nav>

2cfd5cd7767b3804bf25a79b3f2ebee4340285b5
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-02-23 15:02:47 +0200

    Add logic to close dropdown on click away and change page

--- components/DropdownMenu.jsx
+++ components/DropdownMenu.jsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { FaBars } from "react-icons/fa";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { navLinks } from './LinkList';
 
 const DropdownMenu = () => {
@@ -12,27 +12,44 @@ const DropdownMenu = () => {
     setDropdownOpen(!isDropdownOpen);
   }
 
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    window.addEventListener('click', handleClickOutside);
+  }, []);
+
   return (
     <>
-      <div
+      <button
         className={`dropdownIcon 
           ${isDropdownOpen ? 'bg-[#EDF2F7] dark:bg-[#323235]' 
           : ''}`}
-        onClick={handleDropdownToggle}>
+        onClick={handleDropdownToggle}
+        ref={dropdownRef}>
         <FaBars size={14} />
+      </button>
+      <div className={`dropdown ${isDropdownOpen ? 'opacity-100 scale-100' : 
+        'opacity-0 scale-95 hidden'}`}>
+        {isDropdownOpen && (
+          <>
+            {navLinks.map((link, index) => (
+              <Link 
+                key={index}
+                href={link.href}
+                className={`dropdownLink ${index !== navLinks.length - 1 ? 'mt-2' : 'mb-2'}`}
+              >
+                  {link.content}
+              </Link>
+            ))}
+          </>
+        )}
       </div>
-      {isDropdownOpen && (
-        <div className="dropdown">
-          {navLinks.map((link, index) => (
-            <Link
-              key={index}
-              href={link.href}
-              className={`dropdownLink ${index !== navLinks.length - 1 ? 'mt-2' : 'mb-2'}`}>
-                {link.content}
-            </Link>
-          ))}
-        </div>
-      )}
     </>
   )
 }

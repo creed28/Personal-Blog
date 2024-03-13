d0cc0b3df063b749bea10e9ddb55fda2c0c8ef17
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-03-13 09:51:31 +0200

    Add Posts and Single Posts pages

---
+++ app/posts/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import AnimatedPage from "../../../components/AnimatedPage"
+
+const SinglePost = () => {
+  return (
+    <AnimatedPage>
+      Single Post
+    </AnimatedPage>
+  )
+}
+
+export default SinglePost
---
+++ app/posts/page.jsx
@@ -0,0 +1,11 @@
+import AnimatedPage from '../../components/AnimatedPage';
+
+const Posts = () => {
+  return (
+    <AnimatedPage>
+      Posts
+    </AnimatedPage>
+  )
+}
+
+export default Posts
